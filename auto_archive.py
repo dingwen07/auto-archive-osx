37be6372fb3d0599d8658118716905c17f955f13
Dingwen Wang <wangdingwen32@gmail.com>
2024-01-06 06:58:38 -0500

    added logging and tested correctness

--- auto_archive.py
+++ auto_archive.py
@@ -9,7 +9,8 @@ import subprocess
 
 ARCHIVE_FOLDER = 'Archive'
 ARCHIVE_THRESHOLD = 30 # days
-ERROR_LOG_FILE = os.path.join(tempfile.gettempdir(), 'autoarchive_error.log')
+ERROR_LOG_FILE = os.path.join(tempfile.gettempdir(), 'auto_archive.log')
+LOG_LEVEL = 'INFO' # INFO, ERROR
 
 def get_path():
     return os.path.dirname(__file__)
@@ -22,15 +23,21 @@ def get_path_osx():
             return get_path()
     return path
 
-def err_log(e):
-    # save error to debug log
+def err_log(msg, log_type='ERROR'):
+    if log_type == 'INFO' and LOG_LEVEL == 'ERROR':
+        return
     with open(ERROR_LOG_FILE, 'a', encoding='utf-8') as f:
-        f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + ' ' + 'Error: {}\n'.format(e))
+        if msg == '' and log_type == 'INFO':
+            f.write('\n')
+        else:
+            f.write(f"{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())} {log_type}: {msg}\n")
 
 def revert(run_log, target_dir, archive_folder=ARCHIVE_FOLDER):
+    err_log(f'Reverting {run_log}', log_type='INFO')
     try:
         with open(run_log, 'r') as f:
             log = json.load(f)
+            err_log(f'Run log: {log}', log_type='INFO')
     except Exception as e:
         err_log(e)
         return
@@ -39,6 +46,7 @@ def revert(run_log, target_dir, archive_folder=ARCHIVE_FOLDER):
         try:
             src = os.path.join(target_dir, archive_folder, item['dest'])
             dest = os.path.join(target_dir, item['src'])
+            err_log(f'Reverting {src} to {dest}', log_type='INFO')
             if os.path.isdir(src):
                 shutil.copytree(src, dest)
             else:
@@ -46,16 +54,21 @@ def revert(run_log, target_dir, archive_folder=ARCHIVE_FOLDER):
         except Exception as e:
             err_log(e)
             continue
+    
+    err_log('Revert done', log_type='INFO')
 
 
 if __name__ == '__main__':
+    err_log('Process started', log_type='INFO')
     # Mac OS Date Added
     osx_date_added = True
     by_osx_date_added = True
     # get target directory and self name
     if platform.system() == 'Darwin':
+        err_log('Running on OSX', log_type='INFO')
         target_dir = get_path_osx()
         if target_dir.endswith('.app'):
+            err_log('Running in OSX app bundle', log_type='INFO')
             self_name = os.path.basename(target_dir)
             target_dir = os.path.dirname(target_dir)
         else:
@@ -88,7 +101,7 @@ if __name__ == '__main__':
         err_log(e)
         exit()
     
-    print('Config: {}'.format(config))
+    err_log(f'Config: {config}', log_type='INFO')
 
     archive_folder = config['archive_folder']
     archive_threshold = config['archive_threshold']
@@ -96,29 +109,37 @@ if __name__ == '__main__':
     ignore_list = config['ignore']
     check_access_time = config['check_access_time']
     by_osx_date_added = by_osx_date_added and config['by_osx_date_added'] # so it always be false on other platforms
+    if 'log_level' in config:
+        LOG_LEVEL = config['log_level']
+    else:
+        LOG_LEVEL = 'ERROR'
 
     # if in debug mode, set archive folder to Archive_Debug
     # and archive threshold to 0 day
     if debug_mode:
         archive_folder = 'Archive_Debug'
-        archive_threshold = 0
+        if 'debug_archive_threshold' in config:
+            archive_threshold = config['debug_archive_threshold']
+        else:
+            archive_threshold = 0
+        if 'debug_disable_check_osx_date_added' in config:
+            osx_date_added = not config['debug_disable_check_osx_date_added']
         # also, save target_dir and self_name to file
         # and config
-        with open(ERROR_LOG_FILE, 'a', encoding='utf-8') as f:
-            dbgmsg_prefix = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + ' ' + 'Debug: '
-            f.write('\n')
-            f.write(dbgmsg_prefix + 'target_dir: {}\n'.format(target_dir))
-            f.write(dbgmsg_prefix + 'self_name: {}\n'.format(self_name))
-            f.write(dbgmsg_prefix + 'config: {}\n'.format(config))
-            f.write('\n')
+        err_log('target_dir: {}'.format(target_dir), log_type='Debug')
+        err_log('self_name: {}'.format(self_name), log_type='Debug')
+        err_log('config: {}'.format(config), log_type='Debug')
     
     # check if revert mode
     if 'revert' in config:
         if os.path.exists(config['revert']):
+            err_log('Revert mode', log_type='INFO')
             revert(config['revert'], target_dir, archive_folder)
             # move the json to reverted.json
             file_name, file_ext = os.path.splitext(config['revert'])
             shutil.move(config['revert'], file_name + '_reverted' + file_ext)
+            err_log('Process ended', log_type='INFO')
+            err_log('', log_type='INFO')
             exit()
 
     # check if archive folder exists
@@ -128,6 +149,8 @@ if __name__ == '__main__':
             os.mkdir(archive_dir)
         except Exception as e:
             err_log(e)
+            err_log('Process ended', log_type='INFO')
+            err_log('', log_type='INFO')
             exit()
 
 
@@ -135,21 +158,25 @@ if __name__ == '__main__':
     # except self and archive_folder
     # if the Last Modified Time is older than archive_threshold days
     # move it to yyyy-mm folder under archive_folder
+    ignore_list.append(self_name)
+    ignore_list.append(archive_folder)
     moved_files = []
     start_time = time.time()
     for file in os.listdir(target_dir):
-        ignore_list.append(self_name)
-        ignore_list.append(archive_folder)
         if file in ignore_list:
+            err_log(f'Ignored: {file}', log_type='INFO')
             continue
         file_path = os.path.join(target_dir, file)
+        err_log(f'Checking: {file_path}', log_type='INFO')
         file_stat = os.stat(file_path)
+        err_log(f'File stat: {file_stat}', log_type='INFO')
         file_modified_time = file_stat.st_mtime
         file_added_time = file_modified_time
-        if osx_date_added:
+        if osx_date_added or by_osx_date_added:
             try:
                 file_added_time_str = subprocess.check_output(['mdls', '-name', 'kMDItemDateAdded', '-raw', file_path]).decode('utf-8').strip()
                 file_added_time = time.mktime(time.strptime(file_added_time_str, '%Y-%m-%d %H:%M:%S %z'))
+                err_log(f'File added time (OSX): {file_added_time}', log_type='INFO')
             except Exception as e:
                 err_log(e)
         # determine file time
@@ -157,16 +184,20 @@ if __name__ == '__main__':
             file_time = file_added_time
         else:
             file_time = file_modified_time
+        err_log(f'File time: {file_time}', log_type='INFO')
         # determine file threshold time, which one is older
         file_threshold_time = file_modified_time
         if osx_date_added:
             # if file added time is newer than modified time
             # use added time
             file_threshold_time = max(file_modified_time, file_added_time)
+            err_log(f'osx_date_added: file threshold time: {file_threshold_time}', log_type='INFO')
         if check_access_time:
             file_access_time = file_stat.st_atime
             file_threshold_time = max(file_threshold_time, file_access_time)
-
+            err_log(f'check_access_time: file threshold time: {file_threshold_time}', log_type='INFO')
+        
+        err_log(f'Final file threshold time: {file_threshold_time}', log_type='INFO')
         if time.time() - file_threshold_time > archive_threshold * 24 * 60 * 60:
             success = True
             err = None
@@ -174,6 +205,7 @@ if __name__ == '__main__':
                 # move file to archive folder
                 file_archive_dir_relative = time.strftime('%Y-%m', time.localtime(file_time))
                 file_archive_dir = os.path.join(archive_dir, file_archive_dir_relative)
+                err_log(f'File archive dir: {file_archive_dir}', log_type='INFO')
                 if not os.path.exists(file_archive_dir):
                     try:
                         os.mkdir(file_archive_dir)
@@ -182,6 +214,7 @@ if __name__ == '__main__':
                 # check if file with same name exists in archive folder
                 # if so, append a number to the file name, but keep extension
                 file_archive_path = os.path.join(file_archive_dir, file)
+                err_log(f'Init file archive path: {file_archive_path}', log_type='INFO')
                 file_relative = file
                 if os.path.exists(file_archive_path):
                     file_name, file_ext = os.path.splitext(file)
@@ -192,6 +225,7 @@ if __name__ == '__main__':
                         if not os.path.exists(file_archive_path):
                             break
                         i += 1
+                err_log(f'Final file archive path: {file_archive_path}', log_type='INFO')
                 shutil.move(file_path, file_archive_path)
                 # success
                 moved_files.append({
@@ -219,6 +253,8 @@ if __name__ == '__main__':
                     f.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + ' ' + file + ' ' + errmsg + '\n')
             except Exception as e:
                 err_log(e)
+        else:
+            err_log(f'File {file} not old enough', log_type='INFO')
 
     end_time = time.time()
     # log to archive_folder\LOG\RUN\yyyy-mm-dd-HH-MM-SS.json
@@ -238,3 +274,6 @@ if __name__ == '__main__':
             }, f, indent=4)
     except Exception as e: 
         err_log(e)
+    
+    err_log('Process ended', log_type='INFO')
+    err_log('', log_type='INFO')
